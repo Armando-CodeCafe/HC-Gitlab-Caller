59a7a22b1629236514c412526f91fc8e4035b0a1
TheExiledCat <kazekage2002@hotmail.com>
2023-12-13 13:22:15 +0100

    feat(apicalls): Added possibility to get all members in a class

---
+++ caller.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import requests
+import json
+load_dotenv(".env")
+groups = {
+    
+    "22A":os.environ.get("SD22_A"),
+    "22B":os.environ.get("SD22_A"),
+    "23":os.environ.get("SD23"),
+}
+url = "https://git.nexed.com/api/v4/"
+api_key = str(os.environ.get("API_KEY"))
+urlstring = f"{url}groups/{groups['22A']}/members?private_token={api_key}"
+print(urlstring)
+
+membersSD22A= requests.get(urlstring)
+
+with open("members.json", "w") as file:
+    file.write(json.dumps(membersSD22A.json()))
